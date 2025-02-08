bab0c0d40d457fba97a52256f436dd1a0bb5003b
chefdude99 <chefdude.1999@gmail.com>
2025-02-08 00:11:45 +0000

    Proyecto listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 import "bootstrap";
 import "./style.css";
 
+function excusaRandom (array) {
+  let arrayRandom = Math.floor(Math.random() * array.length) 
+  return array[arrayRandom] 
+  }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function () {
+  let quien = ['Dalila', 'Turron', 'Akira', 'Liza'];
+  let accion = ['daño', 'se orino', 'comio', 'escondio'];
+  let que = ['mi carro', 'mi computadora', 'mi vida'];
+  let cuando = ['luego de entrenar', 'mientras viajaba', 'durante mis horas de trabajo', 'en el almuerzo', 'mientras lloraba'];
+  let quienRandom = excusaRandom(quien)
+  console.log(quienRandom)
+  let accionRandom = excusaRandom(accion)
+  console.log(accionRandom)
+  let queRandom = excusaRandom(que)
+  console.log(queRandom)
+  let cuandoRandom = excusaRandom(cuando)
+  console.log(cuandoRandom)
+  let excusa = quienRandom + " " + accionRandom + " " + queRandom + " " + cuandoRandom;
+  document.getElementById("excuse").innerText = excusa; 
+}; 
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
+ 
+
+
+
